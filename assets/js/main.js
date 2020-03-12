9082d7f0541f0395e8041fb3c9918d303a45220b
Mark McClean <mcclean_mark_02@hotmail.com>
2020-03-12 21:40:31 +0000

    Further development of the buildQuiz function. The first question and answers now appear in the quiz form when the start quiz button is clicked. The callout also disappears with this function. New colour scheme chosen for the home page, nav and footer elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,42 +10,36 @@ const answerFive = document.getElementsByClassName('ans5');
 const myQuestions = [
     {
         question: "Starting with the most put these countries in order of total population",
-        answers: {
-            5: "Ireland",
-            2: "United States",
-            3: "Russia",
-            1: "China",
-            4: "United Kingdom"            
-        },
-        correctOrder: "1, 2, 3, 4, 5"
+        answers: [
+            {"correct_order": 5, "details": "Ireland"},
+            {"correct_order": 2, "details": "United States"},
+            {"correct_order": 3, "details": "Russia"},
+            {"correct_order": 1, "details": "China"},
+            {"correct_order": 4, "details": "United Kingdom"}     
+        ]
     },
     {
         question: "Starting with the most put these countries in order of size",
-        answers: {
-            2: "Canada",
-            5: "Ireland",
-            3: "Australia",
-            4: "Mexico",
-            1: "Russia"
-        },
-        correctOrder: "1, 2, 3, 4, 5"
+        answers: [
+            {"correct_order": 2, "details": "Canada"},
+            {"correct_order": 5, "details": "Ireland"},
+            {"correct_order": 3, "details": "Australia"},
+            {"correct_order": 4, "details": "Mexico"},
+            {"correct_order": 1, "details": "Russia"}
+        ]
     }
+    
 ]
 
-$(".game-start").click(function buildQuiz() {
+document.getElementById("start-quiz").onclick = function() {buildQuiz()};
 
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-            for(number in currentQuestion.answers){
-                answers.push(
-                    quizQuestion.innerHTML = question,
-                    answerOne.innerHTML = answers('1'),
-                    answerTwo.innerHTML = answers('2'),
-                    answerThree.innerHTML = answers('3'),
-                    answerFour.innerHTML = answers('4'),
-                    answerFive.innerHTML = answers('5')
-                )
-            }
-        }
-    )
-})
+function buildQuiz() {
+    document.getElementById("callout").classList.add("d-none");
+    document.getElementById("quiz").classList.remove("d-none");
+    document.getElementById("question").innerHTML = myQuestions[0].question;
+    document.getElementById("ans1").innerHTML = myQuestions[0].answers[0].details;
+    document.getElementById("ans2").innerHTML = myQuestions[0].answers[1].details;
+    document.getElementById("ans3").innerHTML = myQuestions[0].answers[2].details;
+    document.getElementById("ans4").innerHTML = myQuestions[0].answers[3].details;
+    document.getElementById("ans5").innerHTML = myQuestions[0].answers[4].details;
+}
