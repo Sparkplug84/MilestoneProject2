ef89961405d4bc36b45f74c71faafa328530ca7f
Mark McClean <mcclean_mark_02@hotmail.com>
2020-03-17 21:58:36 +0000

    In the quiz form, the answers can now be rearranged using an event listener. The buttons used to switch the answers have been changed from 2 buttons to 1 for each answer swap, cleaner for the code and less buttons on the page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
 /* Variables */
 
-const quizQuestion = document.getElementsByClassName('quiz-question');
-const answerOne = document.getElementsByClassName('ans1');
-const answerTwo = document.getElementsByClassName('ans2');
-const answerThree = document.getElementsByClassName('ans3');
-const answerFour = document.getElementsByClassName('ans4');
-const answerFive = document.getElementsByClassName('ans5');
+const quizQuestion = document.getElementById('question');
+const answerOne = document.getElementById('ans1');
+const answerTwo = document.getElementById('ans2');
+const answerThree = document.getElementById('ans3');
+const answerFour = document.getElementById('ans4');
+const answerFive = document.getElementById('ans5');
+const answerOneContainer = document.getElementById('container1');
+const answerTwoContainer = document.getElementById('container2');
+const answerThreeContainer = document.getElementById('container3');
+const answerFourContainer = document.getElementById('container4');
+const answerFiveContainer = document.getElementById('container5');
 
 const myQuestions = [
     {
@@ -36,10 +41,40 @@ document.getElementById("start-quiz").onclick = function() {buildQuiz()};
 function buildQuiz() {
     document.getElementById("callout").classList.add("d-none");
     document.getElementById("quiz").classList.remove("d-none");
-    document.getElementById("question").innerHTML = myQuestions[0].question;
-    document.getElementById("ans1").innerHTML = myQuestions[0].answers[0].details;
-    document.getElementById("ans2").innerHTML = myQuestions[0].answers[1].details;
-    document.getElementById("ans3").innerHTML = myQuestions[0].answers[2].details;
-    document.getElementById("ans4").innerHTML = myQuestions[0].answers[3].details;
-    document.getElementById("ans5").innerHTML = myQuestions[0].answers[4].details;
+    quizQuestion.innerHTML = myQuestions[0].question;
+    answerOne.innerHTML = myQuestions[0].answers[0].details;
+    answerTwo.innerHTML = myQuestions[0].answers[1].details;
+    answerThree.innerHTML = myQuestions[0].answers[2].details;
+    answerFour.innerHTML = myQuestions[0].answers[3].details;
+    answerFive.innerHTML = myQuestions[0].answers[4].details;
 }
+
+document.getElementById("switch1").addEventListener('click', () => {
+  answerTwoContainer.appendChild(answerOneContainer.firstElementChild);
+  answerOneContainer.appendChild(answerTwoContainer.firstElementChild);
+});
+
+document.getElementById("switch2").addEventListener('click', () => {
+  answerThreeContainer.appendChild(answerTwoContainer.firstElementChild);
+  answerTwoContainer.appendChild(answerThreeContainer.firstElementChild);
+});
+
+document.getElementById("switch3").addEventListener('click', () => {
+  answerFourContainer.appendChild(answerThreeContainer.firstElementChild);
+  answerThreeContainer.appendChild(answerFourContainer.firstElementChild);
+});
+
+document.getElementById("switch4").addEventListener('click', () => {
+  answerFiveContainer.appendChild(answerFourContainer.firstElementChild);
+  answerFourContainer.appendChild(answerFiveContainer.firstElementChild);
+});
+
+/* document.getElementById("down1").onclick = function() {switchElements()};
+
+function switchElements() {
+    answerTwoContainer.firstChild.append(answerOneContainer);
+    /* answerOneContainer.firstChild.append(answerTwoContainer.firstChild); 
+    $('#container1:first').appendTo( $('#container2') );
+}*/
+
+console.log("hello!");
